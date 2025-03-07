a03579a4ad575a3bea721563261c30ba34d74d18
shqueel <95152120+shqueel@users.noreply.github.com>
2025-03-07 10:47:08 +0000

    v2, makes more sense to me

--- Scrim/basketball-counter/index.js
+++ Scrim/basketball-counter/index.js
@@ -3,12 +3,12 @@ let home_Score = document.getElementById("home-Score")
 let guest_Score = document.getElementById("guest-Score")
 
 function add(value, num) {
-    if (value == true){
+    if (value == "guest"){
         console.log((num) +" point to guest")
         let score = parseInt(guest_Score.innerText)
         score += (num)
         guest_Score.innerText = score
-    } if(value == null) {
+    } if(value == "home") {
         console.log((num) + " point to home")
         let score = parseInt(home_Score.innerText)
         score += (num)
